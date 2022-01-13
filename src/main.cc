ac3cdf427c8bdb244f9969feb8ecba9ba768c4c5
apre <herculanodavi@gmail.com>
2022-01-13 11:52:47 -0300

    First commit, hello window

---
+++ src/main.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include "window.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+} 
+
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) {
+        std::cout << "Closing window with keyboard command." << "\n";
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main() { 
+    if(!glfwInit()) {
+        std::cout << "Failed to init GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    glViewport(0, 0, 800, 600);
+    while(!glfwWindowShouldClose(window)) {
+        processInput(window);
+        
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0; 
+}
